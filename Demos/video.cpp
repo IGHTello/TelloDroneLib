3a4b47a8b1833e7127349066bedbaa7fc0069e2b
Idan Horowitz <idan.horowitz@gmail.com>
2021-08-25 11:45:35 +0300

    Ask user to start ffplay listener in video demo

--- Demos/video.cpp
+++ Demos/video.cpp
@@ -2,9 +2,14 @@
 #include <iostream>
 
 int main() {
+	std::cout << "Please start an ffplay/ffmpeg listener with: 'ffplay -probesize 32 -sync ext -framerate 35 udp://127.0.0.1:9999' and press any key to continue" << std::endl;
+	std::cin.ignore();
+
 	TelloDrone drone;
+	std::cout << "Connecting to the drone..." << std::endl;
 	drone.wait_until_connected();
 	std::cout << "Connected to the drone! waiting for a minute..." << std::endl;
 	std::this_thread::sleep_for(std::chrono::minutes(1));
+
 	std::cout << "Disconnecting..." << std::endl;
 }
