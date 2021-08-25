2b1b08c7c51b63531f846aff9e404323c37e639e
Idan Horowitz <idan.horowitz@gmail.com>
2021-08-25 13:53:29 +0300

    Add sleep in drone_info
    This lets the initialization sequence finish, so we don't send two requests for each

--- Demos/drone_info.cpp
+++ Demos/drone_info.cpp
@@ -6,9 +6,10 @@ int main()
     Tello::Drone drone;
     drone.wait_until_connected();
     std::cout << "Connected to the drone! Drone Info:" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5)); // Lets the initialization sequence finish, so we don't send two requests for each
     std::cout << "Firmware Version: " << drone.get_firmware_version() << std::endl;
     std::cout << "Loader Version: " << drone.get_loader_version() << std::endl;
-    std::cout << "Bitrate: " << drone.get_bitrate() << std::endl;
+    std::cout << "Bitrate: " << (int)drone.get_bitrate() << std::endl;
     std::cout << "Flight Height Limit: " << drone.get_flight_height_limit() << std::endl;
     std::cout << "Low Battery Warning: " << drone.get_low_battery_warning() << std::endl;
     std::cout << "Attitude Angle: " << drone.get_attitude_angle() << std::endl;
